77cd4211561fd95674cb213d572d28c63c024b14
刘永强 <1532496418@qq.com>
2020-09-05 14:10:48 +0800

    测试target

--- src/main/java/com/atguigu/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.atguigu.service;
-
-import com.atguigu.domain.User;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-public interface UserService{
-    List<User> findAll();
-}
